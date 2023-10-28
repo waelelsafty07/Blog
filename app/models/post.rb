64b982204964897b5d1938c5a3cd95bae5c9dc0c
waelelsafty07 <waelelsafty07@gmail.com>
2023-10-28 20:43:07 +0200

    Fi

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,7 +13,7 @@ class Post < ApplicationRecord
   # Callbacks
   after_save :update_user_posts_counter
 
-  # Validation
+  # Validations
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
   validates :likes_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }

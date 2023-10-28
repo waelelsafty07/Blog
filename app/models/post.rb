d17b30b4ec42f2f40f2e3f6c4ac907dcc6083366
waelelsafty07 <waelelsafty07@gmail.com>
2023-10-28 12:38:48 +0200

    Fix Linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,13 +3,13 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
-  
+
   # Attributes
   attribute :title, :string
   attribute :text, :text
   attribute :comments_counter, :integer, default: 0
   attribute :likes_counter, :integer, default: 0
-  
+
   # Callbacks
   after_save :update_user_posts_counter
 
